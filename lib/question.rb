2827406eab91de1db3bca0c5d376f07e8bf937ec
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2016-10-28 16:36:00 -0300

    basic rspec de question

---
+++ lib/question.rb
@@ -0,0 +1,8 @@
+require 'mongo_db'
+
+class Question
+  include MongoDB
+
+  # field :autor, String
+  # field :contenido, String
+end
---
+++ spec/question_spec.rb
@@ -0,0 +1,22 @@
+require 'question'
+require 'spec_helper'
+
+describe Question do
+
+  it 'rspec works' do
+    expect(true).to be true
+  end
+
+  # it 'has automatics accessors' do
+  #   p = Question.new
+  #   p.autor     = 'Eduardo'
+  #   p.contenido = 'Cual es el radio de la tierra'
+  #
+  #   expect(p.autor).to eq 'Eduardo'
+  #   expect(p.contenido).to eq 'Cual es el radio de la tierra'
+  # end
+
+  # it 'does something useful' do
+  #   expect(false).to eq(true)
+  # end
+end
